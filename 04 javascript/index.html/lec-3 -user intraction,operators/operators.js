0406832599b59d48e977bd5af7f879b4536c0c2c
debendra <mrdebendramohanta@gmail.com>
2026-07-21 09:00:55 +0530

    user interaction

---
+++ 04 javascript/index.html/lec-3 -user intraction,operators/operators.js
@@ -0,0 +1,34 @@
+console.log(2**3)
+console.log(4**3)
+
+let x = 10
+x**=2 //x=**2
+console.log(x)
+
+console.log(5 =="5")
+console.log(5 =="4")
+console.log(5 =="")
+console.log(1==true)
+console.log(1==false)
+
+
+console.log(5 === "5")
+console.log(1===true)
+console.log(1===false)
+
+console.log(1!=="1")
+console.log(2!=="3")
+
+// console.log(1!==="1")
+// console.log(1!==="2")
+
+console.log(2 << 3) //4*2^3//32
+console.log(8<<5) //8*2^5//256
+
+console.log(64>>5) //64/2^5//2
+console.log(256>>3) //256/2^3//32
+
+//console.log((2>1)? true:false)
+
+let z= 100, y = 20
+console.log(z > y ? z : y)
---
+++ 04 javascript/index.html/lec-3 -user intraction,operators/user interaction.js
@@ -0,0 +1,23 @@
+//alert("Hello Everyone!")
+//window.alert("Good Morning")
+
+//  let name = prompt("EnterYour Name:-")
+//  console.log(name)
+
+//  let name = prompt("Enter Your Name")
+//  if( name ){
+
+//      console.log("Your name is",name)
+//   } else{
+//     console.log("Re enter user name")
+//     console.log("name is",prompt("Re enter user name"))
+    
+//   }
+ 
+   
+ let choice = confirm("Are you enjoying js classes ?")
+ console.log(choice)
+
+ console.log("Hii")   //white
+ console.warn("Hello")  //yellow
+ console.error("Bye")   //red
